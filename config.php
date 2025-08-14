73a67eb70c9a4fb5a6c884d9b8772a0cdc67b072
maryaalshameri <marynaalshameri@gmail.com>
2025-08-14 18:27:25 +0300

    update config page

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 
 namespace MiniStore;
 
-define('TAX_RATE', 0.15); // 15% ضريبة
-define('DISCOUNT_PERCENT', 0.10); // 10% خصم
+define('TAX_RATE', 0.15); // 15% TAX
+define('DISCOUNT_PERCENT', 0.10); // 10% DISCOUNT
